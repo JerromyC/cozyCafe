6bdd2e7721e573ea8cd8a9ea4f309dea8783e8d4
JerromyC <59486979+JerromyC@users.noreply.github.com>
2024-01-26 19:58:48 -0600

    Adding code for navbar and header

---
+++ script.js
@@ -0,0 +1,21 @@
+// code for the fixed header 
+window.onscroll = function() {
+    var header = document.getElementById('mainHeader');
+    var logo = header.querySelector('.logo');
+    var page = document.getElementById('page');
+
+    // setting the distance a user needs to scroll for the function to begin
+    var sticky = header.offsetTop + 250;
+
+    if (window.scrollY > sticky) {
+        // adding styles when scroled down
+        header.classList.add('fixed-header');
+        logo.classList.add('hide-logo');
+        page.classList.add('added-padded');
+    } else {
+        // removing styles when scrolling back to the top
+        header.classList.remove('fixed-header');
+        logo.classList.remove('hide-logo');
+        page.classList.remove('added-padded');
+    }
+};
